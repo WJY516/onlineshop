b46e4e2768ef2c91ff2f8bab7fcd3e1cfad4dc1b
wujiayi <wujiayi0924@qq.com>
2018-07-29 14:22:18 +0800

    新增按类型查找商品

--- src/main/java/com/service/GoodsService.java
+++ src/main/java/com/service/GoodsService.java
@@ -19,6 +19,7 @@ public interface GoodsService {
 	public TbGoods queryGoodsById(int goodsId);
 	public List<TbGoods> queryGoodsByName(String name);
 	public List<TbGoods> queryGoodsAll();
+	public List<TbGoods> queryGoodsByType(String name);
 	public int insertGoods(TbGoods tbgoods);
 	public int updateGoods(TbGoods tbgoods);
 	public int deleteGoodsById(int goodsId);
