1320622dee0c17aea4e30142dab0120593455ee7
1477701833 <1477701833@qq.com>
2018-07-30 14:05:18 +0800

    0730zqd

--- src/main/java/com/controller/SubscribeController.java
+++ src/main/java/com/controller/SubscribeController.java
@@ -1,5 +1,7 @@
 package com.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +16,18 @@ public class SubscribeController {
 	
 	@Autowired
 	TbSubscribeService subscribeService;
-	@RequestMapping("/subscribe")
+	
+	public String selectSubscribe(HttpSession session){
+		String userName = (String) session.getAttribute("username");
+		subscribeService.selectSubscribe(userName);
+		return "/person/collection.jsp";
+	}
+	@RequestMapping("/insertSubscribe")
 	//http://127.0.0.1/onlineshop/subscribe/subscribe?username=111&goodsId=001
 	public String subscribe(TbSubscribeKey subscribe){
-		subscribeService.subscribe(subscribe);
-		return null;
-		
+		subscribeService.insertSubscribe(subscribe);
+		return null;	
 	}
+	
 
 }
--- src/main/java/com/service/TbSubscribeService.java
+++ src/main/java/com/service/TbSubscribeService.java
@@ -7,7 +7,7 @@ import com.domain.TbSubscribeKey;
 
 public interface TbSubscribeService {
 	
-	public void subscribe(TbSubscribeKey subscribe);//关注商品
+	public void insertSubscribe(TbSubscribeKey subscribe);//关注商品
 	public void deleteSubscribe(String userName,int goodsId);//取消关注
 	public List<TbGoods> selectSubscribe(String userName);//查关注商品列表
 }
