24912728bd38152f24199bdae0144dbe8c5732a0
wujiayi <wujiayi0924@qq.com>
2018-08-01 09:34:11 +0800

    被覆盖部分重传

--- src/main/java/com/controller/AdminController.java
+++ src/main/java/com/controller/AdminController.java
@@ -3,8 +3,11 @@
  */
 package com.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,7 +17,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.domain.TbBrand;
 import com.domain.TbGoods;
+import com.service.BrandService;
 import com.service.GoodsService;
 
 /**
@@ -27,11 +32,37 @@ import com.service.GoodsService;
 public class AdminController {
 	@Autowired
 	GoodsService goodsService;
+	@Autowired
+	BrandService brandService;
 	
 	@RequestMapping("/goods")
 	public String goodsqueryAll(Model m){
 		List<TbGoods> goodsListAll = goodsService.queryGoodsAll();
 		m.addAttribute("goodsListAll", goodsListAll);
 		return "../admin/index.jsp";
+	}
+	
+	@RequestMapping("/addgoods")
+//	@ResponseBody
+	public void goodsAdd(TbGoods tbgoods,TbBrand tbbrand,HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
+//		System.out.println("goodsAdd");
+//		System.out.println("tbgoods:"+tbgoods.getGoodsName());
+//		System.out.println("tbbrand:"+tbbrand.getBrandName());
+//		List<Object> list = new ArrayList<Object>();
+//		list.add(tbgoods);
+//		list.add(tbbrand);
+		/*
+		 * 此处应该考虑数据同步更新和回滚。
+		 * 这个需求之后再做了
+		 */
+		int in_flag = -2;
+		in_flag += goodsService.insertGoods(tbgoods);
+		int goodsId = goodsService.getGoodsId(tbgoods);
+		tbbrand.setGoodsId(String.valueOf(goodsId));
+		tbbrand.setType1("type1");
+		tbbrand.setType2("type2");
+		in_flag += brandService.insertBrands(tbbrand);
+		request.setAttribute("in_flag", in_flag);
+		request.getRequestDispatcher("../admin/addGoods.jsp").forward(request, response);
 	}
 }
